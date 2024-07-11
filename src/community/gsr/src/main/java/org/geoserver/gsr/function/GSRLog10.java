658f7b1e57cc24e558b875e697d381f2a5dac294
Sooyong Kim <sooyong.kim@koordinates.com>
2024-07-12 09:44:08 +1200

    gsr: Register remaining (possible) numeric and date SQL functions - SQL function LOG10 doesn't exist in geoserver, but Math has log10(). New custom function GSRLog10 to implement a function that evaluates using Math.log10

---
+++ src/community/gsr/src/main/java/org/geoserver/gsr/function/GSRLog10.java
@@ -0,0 +1,41 @@
+package org.geoserver.gsr.function;
+
+import java.util.List;
+import org.geotools.filter.FunctionImpl;
+import org.geotools.filter.capability.FunctionNameImpl;
+import org.geotools.util.Converters;
+import org.geotools.util.factory.Hints;
+import org.opengis.feature.type.Name;
+import org.opengis.filter.expression.Expression;
+import org.opengis.filter.expression.Literal;
+
+/** LOG10(<number>): The base-10 logarithm of the specified number. */
+public class GSRLog10 extends FunctionImpl {
+
+    public GSRLog10(Name name, List<Expression> args, Literal fallback) {
+        functionName = new FunctionNameImpl(name, args.size());
+        setName(name.getLocalPart());
+        setFallbackValue(fallback);
+        setParameters(args);
+    }
+
+    @Override
+    public Object evaluate(Object object) {
+
+        Object arg0;
+
+        arg0 = getParameters().get(0).evaluate(object);
+
+        if (arg0 == null) {
+            return null;
+        }
+
+        arg0 = Converters.convert(arg0, Double.class, new Hints());
+        if (arg0 == null) {
+            throw new IllegalArgumentException(
+                    "Filter Function problem for function log argument #0 - expected type double");
+        }
+
+        return Math.log10((Double) arg0);
+    }
+}
